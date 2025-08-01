f20d1074dfed010732e9494e5b03e676e8ee80c8
Saulo Fernandes Araújo Silva <saulofernandesarasilva@gmail.com>
2025-08-01 16:11:38 -0300

    Create exercício003.py

---
+++ exercício003.py
@@ -0,0 +1,7 @@
+#Exercício Python 3: Crie um programa que leia dois números e mostre a soma entre eles.
+
+n1 = int(input('Digite um número para soma:'))
+n2 = int(input('Digite outro número:'))
+s = n1 + n2
+
+print('A soma entre {} + {} = {}' .format(n1,n2,s))
