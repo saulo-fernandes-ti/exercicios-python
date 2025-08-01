92bd7127aeae4767ef2d7e0efcf2192d4d71bc89
Saulo Fernandes Araújo Silva <saulofernandesarasilva@gmail.com>
2025-08-01 16:04:08 -0300

    Create exercício002.py

---
+++ exercício002.py
@@ -0,0 +1,5 @@
+#Exercício Python 2: Faça um programa que leia o nome de uma pessoa e mostre
+#uma mensagem de boas-vindas.
+
+n = input('Digite seu nome:')
+print('Seja muito bem vindo {}' .format(n))
